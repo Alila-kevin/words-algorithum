26845fdf17c8935ad4424138a186a3aff62df53d
unknown <Administrator@ICTINTERN-PC.Billserver.kiwasco.co.ke>
2023-06-15 14:22:13 +0300

    word algorithum

---
+++ project15m6/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("please enter the number of word to be counted");
+        string input = Console.ReadLine();
+
+        int letterCount = CountLetters(input);
+        Console.WriteLine("Number of letters: " + letterCount);
+
+        // Additional code can be added here
+        //string input = "Hello, World! This is a sample sentence.";
+
+        int wordCount = CountWords(input);
+        Console.WriteLine("Number of words: " + wordCount);
+
+        // Additional code can be added here
+    }
+    public static int CountLetters(string input)
+    {
+        int count = 0;
+        foreach (char c in input)
+        {
+            if (char.IsLetter(c))
+                count++;
+        }
+        return count;
+    }
+
+    public static int CountWords(string input)
+    {
+        string[] words = input.Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+        return words.Length;
+    }
+}
